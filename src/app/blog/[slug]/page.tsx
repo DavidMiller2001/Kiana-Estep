7b55f405b27bff999e0263455e3a860424640aa4
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-01-31 19:49:45 -0500

    add loading state to dynamic pages

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { api } from "~/trpc/react";
 
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center p-8 text-3xl font-bold">
+      <h2>Loading...</h2>
+    </div>
+  );
+};
+
 const Page = ({ params }: { params: { slug: string } }) => {
   const decodedSlug = decodeURI(params.slug);
-  console.log(decodedSlug);
-  const { data: post, isError } = api.post.getOne.useQuery({
+  const {
+    data: post,
+    isError,
+    isLoading,
+  } = api.post.getOne.useQuery({
     title: decodedSlug,
   });
 
+  if (isLoading) {
+    return <Loading />;
+  }
   if (isError || !post) {
     return <h1>Post not found :(</h1>;
   }
